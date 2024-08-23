fbefc281370dabcc7e67e45cd1b37a0d31e568e6
Marco Perberschlager <marco.perberschlager@pimcore.com>
2024-08-23 11:49:50 +0200

    121 Changed character set for column

---
+++ src/Migrations/Version20240823111615.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace OutputDataConfigToolkitBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20240823111615 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Migrate bundle_outputdataconfigtoolkit_outputdefinition table columns';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $table = $schema->getTable('bundle_outputdataconfigtoolkit_outputdefinition');
+
+        $query = "ALTER TABLE `%s` CHANGE COLUMN `configuration` `configuration` LONGTEXT
+                    CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_bin' NULL DEFAULT NULL;";
+
+        $this->addSql(sprintf($query, $table->getName()));
+    }
+
+    public function down(Schema $schema): void
+    {
+        $table = $schema->getTable('bundle_outputdataconfigtoolkit_outputdefinition');
+
+        $query = "ALTER TABLE `%s` CHANGE COLUMN `configuration` `configuration` LONGTEXT
+                    CHARACTER SET 'latin1' NULL DEFAULT NULL;";
+
+        $this->addSql(sprintf($query, $table->getName()));
+    }
+}
